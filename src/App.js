282a89afd1247994cd6991428b558f8e83b9eb21
Bill Trikalinos <billtrik@gmail.com>
2017-06-07 21:26:22 +0200

    Create dummy views
    -Homepage
    -Dictionary details view

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Provider} from 'react-redux';
 import {
   BrowserRouter as Router,
   Route,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 import buildStore from './store';
 import Layout from './components/layout';
-import logo from './logo.svg';
-import './App.css';
+import Homepage from './pages/homepage';
+import DictionaryDetails from './pages/dictionary-details';
 
 const store = buildStore();
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
 export default () => (
   <Provider store={store}>
     <Router>
       <Layout>
-        <Route exact path="/" component={App}/>
+        <Route exact path="/" component={Homepage}/>
+        <Route exact path="/dictionary/:id" component={DictionaryDetails}/>
       </Layout>
     </Router>
   </Provider>
